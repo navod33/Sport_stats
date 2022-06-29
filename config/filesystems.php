62f20045baa812574cd8f192ba85d1f8bd314663
Shane Perera <shane7@gmail.com>
2022-06-29 12:35:30 +1000

    Split image upload disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,6 +42,12 @@ return [
             'visibility' => 'public',
         ],
 
+        'user_uploads' => [
+            'driver' => 'local',
+            'root' => storage_path('app/user_uploads'),
+            // 'url' => env('APP_URL').'/storage',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
